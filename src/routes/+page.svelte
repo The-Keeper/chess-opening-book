872c69d2c9e0b0ae06164d14606c66f6dcbd7c6a
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-09-29 08:25:39 +0500

    components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,41 @@
 <script lang="ts">
+	import {Chessground} from 'svelte-chessground';
+	import {Chess} from 'chess.js';
+	import {onMount} from 'svelte';
+	import {toDests,playOtherSide} from '$lib/util.js';
+
+
+	const chess = new Chess();
+
+    $: moveNumber = chess.moveNumber();
+	let chessground: any;
+
+	let config = {
+		movable: {
+			color: 'white',
+			free: false,
+			dests: toDests(chess),
+		}
+	};
+
+	onMount(async () => {
+		chessground.set( {
+			movable: { events: { after: playOtherSide( chessground, chess ) } }
+		});
+	});
+
 </script>
 
+<div class="container" id="board">
+    <Chessground bind:this={chessground} {config}/>
+</div>
+<p>
+    It's move {moveNumber}.
+</p>
+
+<style>
+    .container {
+        width: 400px;
+        height: 400px;
+    }
+</style>
