e3ced6d2f3b356dd62a3312b39e56b7b735456c2
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-09-29 20:06:37 +0500

    png

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,14 @@
 	import { Chess } from 'chess.js';
 	import { onMount } from 'svelte';
 	import {toDests, playOtherSide} from '$lib/util';
+	import { parse } from '@mliebelt/pgn-parser';
+	import type { PgnOptions } from '@mliebelt/pgn-parser';
 
 
 	const chess = new Chess();
 
 	$: moveNumber = chess.moveNumber();
+	let parsed = {}
 
 	let chessground: Chessground;
 
@@ -24,13 +27,22 @@
 			movable: { events: { after: playOtherSide(chessground, chess) } }
 		});
 	});
+
+	let pgnToLoad = "";
+	function loadPgn() {
+		let options = undefined as unknown as PgnOptions;  
+	 	parsed = parse(pgnToLoad, options);
+		console.log(parsed);
+	}
 </script>
 
 <div class="container" id="board">
 	<Chessground bind:this={chessground} {config} />
 </div>
+<textarea bind:value={pgnToLoad}></textarea>
+<button on:click={loadPgn}> Load</button>
 <p>
-	It's move {moveNumber}.
+	{ parsed }
 </p>
 
 <style>
