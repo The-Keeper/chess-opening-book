9b1e0855289d233abb407f54a3de2144e1d25cc4
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-10-01 04:34:29 +0500

    debug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,16 @@
 	import { onMount } from 'svelte';
 	import {toDests, playOtherSide} from '$lib/util';
 	import { parse } from '@mliebelt/pgn-parser';
-	import type { PgnOptions } from '@mliebelt/pgn-parser';
+	import type { PgnOptions, ParseTree } from '@mliebelt/pgn-parser';
 
 
 	const chess = new Chess();
 
+	let repertoire = new Chess();
+
 	$: moveNumber = chess.moveNumber();
-	let parsed = {}
+	let parse_options = undefined as unknown as PgnOptions;  
+	$: parsed = parse(pgnToLoad, parse_options) as ParseTree[];
 
 	let chessground: Chessground;
 
@@ -28,22 +31,63 @@
 		});
 	});
 
-	let pgnToLoad = "";
-	function loadPgn() {
-		let options = undefined as unknown as PgnOptions;  
-	 	parsed = parse(pgnToLoad, options);
-		console.log(parsed);
+
+	type GameTree = {
+		parent: GameTree,
+		fen: string,
+		children: GameTree[]
+	};
+
+
+	function makeMoveInto( logic: Chess, gt: GameTree, pt: any ) {
+		const notation = pt?.notation?.notation;
+		// logic.move(notation);
+		// console.log( logic.turn(), logic.fen() );
+		// logic.undo()
+
+		console.log(notation);
+
+		if (pt.variations) {
+			pt.variations.forEach((variation: any) => {
+			makeMoveInto(logic, gt, variation);
+		});
+		}
 	}
+
+	function buildTree() {
+		let result = {} as GameTree;
+		let logic = new Chess()
+		result.fen = logic.fen();
+		if (parsed.length) {
+			let game = parsed[0] as ParseTree;
+			console.log(game);
+			console.log(result.fen)
+			game.moves.forEach(move => {
+				const notation = move.notation.notation;
+				makeMoveInto(logic, result, move);
+			});
+		}
+	}
+
+	let pgnToLoad = `1. e4 e5 (1... Bg6 2. Bf4 ) (1... d5 2. exd5) *`
 </script>
 
 <div class="container" id="board">
 	<Chessground bind:this={chessground} {config} />
 </div>
+<button on:click={buildTree}>log</button>
 <textarea bind:value={pgnToLoad}></textarea>
-<button on:click={loadPgn}> Load</button>
-<p>
-	{ parsed }
-</p>
+
+{#each parsed as game, game_idx}
+<div class="container-fluid">
+	{#each game.moves as move, move_idx}
+		<button on:click={() => console.log(move)} >
+			{ move.notation.notation }
+		</button>
+	{/each}
+</div>
+{/each}
+
 
 <style>
 	.container {
