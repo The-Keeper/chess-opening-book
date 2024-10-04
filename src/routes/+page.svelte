7d902a3b1a4a4abf85e01154f0bd4aed6e8df340
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-10-04 19:35:18 +0500

    datatypes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 
 	const chess = new Chess();
 
-	let repertoire = new Chess();
+	let repertoire: ChessRepertoire;
 
 	$: moveNumber = chess.moveNumber();
 	let parse_options = undefined as unknown as PgnOptions;  
@@ -38,36 +38,33 @@
 	}
 
 	type GameMoveEdge = {
-		notation: String,
-		from: GameStateNode,
-		to: GameStateNode
+		notation: string,
+		from_key: string,
+		to_key: string
 	}
 
-	type GameVariationsData = { 
-		states: GameStateNode[],
+	type ChessRepertoire = {
+		states: Map<string, GameStateNode>;
 		moves: GameMoveEdge[]
 	}
 
-	function addVariationToRepertoire( repertoire: GameVariationsData, logic: Chess, variation: any[], fromNode: GameStateNode ) {
-		let state = fromNode;
+	function addVariationToRepertoire( repertoire: ChessRepertoire, logic: Chess, variation: any[] ) {
 
 		for (let i = 0; i < variation.length; i++) {
 			const move = variation[i];
 			const notation = String(move?.notation?.notation);
 
 			move.variations.forEach((move_var: any[]) => {
-				addVariationToRepertoire(repertoire, logic, move_var, fromNode)
+				addVariationToRepertoire(repertoire, logic, move_var)
 			});
 
-			let oldStateId = state.id;
+			let oldStateKey = keyFromPosition(logic);
+			logic.move(notation);
+			let newStateKey = addPositionToRepertoire(logic, repertoire);
 
-			logic.move(notation)
 
-			state = { fen: logic.fen(), id: repertoire.states.length } 
-			// console.log(state);
-
-			repertoire.states.push(state);
-			let edge: GameMoveEdge = { from: repertoire.states[oldStateId], to: repertoire.states[state.id], notation }
+			
+			let edge: GameMoveEdge = { from_key: oldStateKey, to_key: newStateKey, notation }
 			repertoire.moves.push(edge);
 
 			// console.log({parentId});
@@ -79,21 +76,41 @@
 		}
 	}
 
-	
-	function buildTree() {
-	 	let repertoire: GameVariationsData = { states: [], moves: [] };
-	 	let logic = new Chess();
-        let startingPosition: GameStateNode = { fen: logic.fen(), id: 0 }
+	function addPositionToRepertoire(position: Chess, repertoire: ChessRepertoire) {
+		const key = keyFromPosition(position);
 
-		repertoire.states.push(startingPosition);
+		if (repertoire.states.has(key)) {
+			return key;
+		}
+
+		let state = { fen: position.fen(), id: repertoire.states.size } 
+		repertoire.states.set(key, state)
+		return key;
+
+	}
+
+	function keyFromPosition(position: Chess) {
+		let key = position.fen();
+		return key;
+	}
+	
+	function buildTree(repertoire: ChessRepertoire) {
+	 	repertoire = { states: new Map(), moves: [] };
+	 	let logic = new Chess();
+
+		addPositionToRepertoire(logic, repertoire);
 
 		if (parsed.length) {
 			let game = parsed[0] as ParseTree;
 			console.log(game);
 
-			addVariationToRepertoire(repertoire, logic, game.moves, startingPosition);
+			addVariationToRepertoire(repertoire, logic, game.moves);
 			console.log(repertoire);
 		}
+	}
+
+	function handleLogClick() {
+		buildTree(repertoire);
 	}
 
 	let pgnToLoad = `1. e4 (1. d4 Nf6) 1... e5 2. Nf3 (2. Bc4 f6 3. Nf3 (3. Qh5+ g6 4. Qh3)) (2. d4 exd4) 2... Nc6 3. Bb5`
@@ -102,7 +119,7 @@
 <div class="container" id="board">
 	<Chessground bind:this={chessground} {config} />
 </div>
-<button on:click={buildTree}>log</button>
+<button on:click={handleLogClick}>log</button>
 <textarea bind:value={pgnToLoad}></textarea>
 
 {#each parsed as game, game_idx}
